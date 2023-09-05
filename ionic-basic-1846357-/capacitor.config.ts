f4215fe54f37ceb21ef68abe115c9395556e6bda
Cesar Saucedo <71368732+dlablo33@users.noreply.github.com>
2023-09-04 18:28:26 -0600

    test1
    testt1

---
+++ ionic-basic-1846357-/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-basic-1846357-',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
