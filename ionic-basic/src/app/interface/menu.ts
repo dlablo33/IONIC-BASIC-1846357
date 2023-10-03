f055b87eee97be90ef715b78562c51280f06f944
Cesar Saucedo <71368732+dlablo33@users.noreply.github.com>
2023-10-02 20:36:42 -0600

    cambios

--- ionic-basic/src/app/interface/menu.ts
+++
@@ -1,5 +0,0 @@
-export interface Menu {
-    nombre: string;
-    enlace: string;
-    icono: string;
-}
--- ionic-basic/src/app/interface/receta.ts
+++
@@ -1,6 +0,0 @@
-export interface Receta {
-    id: number;
-    nombre: string; 
-    image: string;
-    ingredientes: string[]; 
-}
